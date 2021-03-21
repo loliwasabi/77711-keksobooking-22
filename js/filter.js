3803fb7dd06d86236bb7f405ba81d8ca21b536dd
loliwasabi <56650672+loliwasabi@users.noreply.github.com>
2021-03-21 15:52:51 +0400

    Объединение фильтров

--- js/filter.js
+++ js/filter.js
@@ -7,7 +7,6 @@ const ANY_GUESTS = 'any';
 
 
 /* common filter */
-
 const filteringHouse = (filterFunctionsArray) => {
   const adMarkers = document.querySelectorAll('.adPins');
   const filteredAds = [];
@@ -16,7 +15,6 @@ const filteringHouse = (filterFunctionsArray) => {
     adMarker.remove();
   }
   adMarkers.forEach(removeAdMarker);
-  console.log('==>' + filterFunctionsArray.length);
 
   adResponse.forEach((adData) => {
     let filteredAd = adData;
@@ -36,15 +34,14 @@ const filteringHouse = (filterFunctionsArray) => {
 
 
 /* array of filter functions */
+const FILTER_FUNCTIONS = [];
 
-const FILTER_FUNCTIONS = []
 
 /* фильтрация по типу */
 const typeFilterSelect = document.querySelector('#housing-type');
 
 typeFilterSelect.addEventListener('change', (evt) => {
   evt.preventDefault();
-
 
   const typeFilter = (adData) => {
     if (adData && (evt.target.value === adData.offer.type || evt.target.value === ANY_TYPE)) {
@@ -54,54 +51,7 @@ typeFilterSelect.addEventListener('change', (evt) => {
   if (!FILTER_FUNCTIONS.includes(typeFilter)) {
     FILTER_FUNCTIONS.push(typeFilter);
   }
-
   filteringHouse(FILTER_FUNCTIONS);
-});
-
-
-/* фильтрация по числу комнат */
-const roomsFilterSelect = document.querySelector('#housing-rooms');
-
-roomsFilterSelect.addEventListener('change', (evt) => {
-  evt.preventDefault();
-
-  const roomFilter = (adData) => {
-    if (adData && (Number(evt.target.value) === adData.offer.rooms || evt.target.value === ANY_ROOMS)) {
-      return adData;
-    }
-  }
-  if (!FILTER_FUNCTIONS.includes(roomFilter)) {
-    FILTER_FUNCTIONS.push(roomFilter);
-  }
-
-  filteringHouse(FILTER_FUNCTIONS);
-});
-
-
-
-/* фильтрация по числу гостей */
-const guestsFilterSelect = document.querySelector('#housing-guests');
-
-guestsFilterSelect.addEventListener('change', (evt) => {
-  evt.preventDefault();
-  const adMarkers = document.querySelectorAll('.adPins');
-  const filteredAds = [];
-
-  const removeAdMarker = (adMarker) => {
-    adMarker.remove();
-  }
-  adMarkers.forEach(removeAdMarker);
-
-  adResponse.forEach((adData) => {
-    if (Number(evt.target.value) === adData.offer.guests || evt.target.value === ANY_GUESTS) {
-      filteredAds.push(adData);
-    }
-  });
-
-  const slicedAdList = sliceAdList(filteredAds);
-  slicedAdList.forEach((adData) => {
-    createAdCard(adData);
-  })
 });
 
 
@@ -120,37 +70,64 @@ const PRICE_RANGE = {
     max: Infinity,
   },
 }
-
 const ANY_PRICE = 'any';
-
 const priceFilterSelect = document.querySelector('#housing-price');
 
 priceFilterSelect.addEventListener('change', (evt) => {
   evt.preventDefault();
-  let filteredAds = [];
-  const selectedPriceValue = evt.target.value;
-  if (!(selectedPriceValue === ANY_PRICE)) {
-    const adMarkers = document.querySelectorAll('.adPins');
 
-    const removeAdMarker = (adMarker) => {
-      adMarker.remove();
-    }
-    adMarkers.forEach(removeAdMarker);
-
-    adResponse.forEach((adData) => {
-      if ((adData.offer.price >= PRICE_RANGE[selectedPriceValue].min && adData.offer.price <= PRICE_RANGE[selectedPriceValue].max)
-      ) {
-        filteredAds.push(adData);
+  const priceFilter = (adData) => {
+    const selectedPriceValue = evt.target.value;
+    if (selectedPriceValue === ANY_PRICE) {
+      return adData;
+    } else {
+      if (adData && (adData.offer.price >= PRICE_RANGE[selectedPriceValue].min) && (adData.offer.price <= PRICE_RANGE[selectedPriceValue].max)) {
+        return adData;
       }
-    });
-  } else {
-    filteredAds = adResponse;
+    }
   }
 
-  const slicedAdList = sliceAdList(filteredAds);
-  slicedAdList.forEach((adData) => {
-    createAdCard(adData);
-  })
+  if (!FILTER_FUNCTIONS.includes(priceFilter)) {
+    FILTER_FUNCTIONS.push(priceFilter);
+  }
+  filteringHouse(FILTER_FUNCTIONS);
+});
+
+
+/* фильтрация по числу комнат */
+const roomsFilterSelect = document.querySelector('#housing-rooms');
+
+roomsFilterSelect.addEventListener('change', (evt) => {
+  evt.preventDefault();
+
+  const roomFilter = (adData) => {
+    if (adData && (Number(evt.target.value) === adData.offer.rooms || evt.target.value === ANY_ROOMS)) {
+      return adData;
+    }
+  }
+  if (!FILTER_FUNCTIONS.includes(roomFilter)) {
+    FILTER_FUNCTIONS.push(roomFilter);
+  }
+  filteringHouse(FILTER_FUNCTIONS);
+});
+
+
+/* фильтрация по числу гостей */
+const guestsFilterSelect = document.querySelector('#housing-guests');
+
+guestsFilterSelect.addEventListener('change', (evt) => {
+  evt.preventDefault();
+
+  const guestsFilter = (adData) => {
+    if (adData && (Number(evt.target.value) === adData.offer.guests || evt.target.value === ANY_GUESTS)) {
+      return adData;
+    }
+  }
+  if (!FILTER_FUNCTIONS.includes(guestsFilter)) {
+    FILTER_FUNCTIONS.push(guestsFilter);
+  }
+
+  filteringHouse(FILTER_FUNCTIONS);
 });
 
 
@@ -160,29 +137,19 @@ const housingFeatures = document.querySelectorAll('#housing-features input');
 housingFeatures.forEach((housingFeature) => {
   housingFeature.addEventListener('change', (evt) => {
     evt.preventDefault();
-    let filteredAds = [];
 
-    if (evt.target.checked) {
-      const adMarkers = document.querySelectorAll('.adPins');
-      const removeAdMarker = (adMarker) => {
-        adMarker.remove();
-      }
-      adMarkers.forEach(removeAdMarker);
-
-      adResponse.forEach((adData) => {
-        if (adData.offer.features.includes(evt.target.value)) {
-          filteredAds.push(adData);
+    const featuresFilter = (adData) => {
+      if (evt.target.checked) {
+        if (adData && adData.offer.features.includes(evt.target.value)) {
+          return adData;
         }
-      })
-    } else {
-      filteredAds = adResponse;
+      } else {
+        return adData;
+      }
     }
-
-    const slicedAdList = sliceAdList(filteredAds);
-    slicedAdList.forEach((adData) => {
-      createAdCard(adData);
-    })
-  })
+    if (!FILTER_FUNCTIONS.includes(featuresFilter)) {
+      FILTER_FUNCTIONS.push(featuresFilter);
+    }
+    filteringHouse(FILTER_FUNCTIONS);
+  });
 })
-
-
